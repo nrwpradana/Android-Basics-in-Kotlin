75ed586c772bee9b524383c448eb6effd28fbf0f
Nadhiar Pradana <nadhiar11@gmail.com>
2022-03-28 23:24:24 +0700

    Add files via upload

---
+++ debugging/app/src/main/java/com/androideradev/www/debugging/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.androideradev.www.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+}
